bbc1e34d15f7fbd94b27502f0889a849c5366d51
curtiss-504 <96351719+curtiss-504@users.noreply.github.com>
2023-11-03 12:59:06 -0700

    Add files via upload

---
+++ lorie.py
@@ -0,0 +1,8 @@
+chenn_karaktè = "Jodia Se Gros Koze Nan La Repiblik"
+miniskil = chenn_karaktè.lower()
+print(miniskil)
+
+
+chenn = "Haiti pap Peri Viv Haiti"
+lis_espas = chenn.split()
+print(lis_espas)
