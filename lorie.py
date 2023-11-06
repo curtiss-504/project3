ab9389fd7bb4b478944388954a7a19c6cb91a919
curtiss-504 <96351719+curtiss-504@users.noreply.github.com>
2023-11-05 18:36:39 -0800

    tree commit

--- lorie.py
+++ lorie.py
@@ -5,15 +5,15 @@ print(miniskil)
 
 chenn = "Haiti pap Peri Viv Haiti"
 lis_espas = chenn.split()
-print(lis_espas)
+print("afiche lis ki gen chak eleman yo",lis_espas)
 
 chenn = "Isit la nou vann poul🐤🐥🐤🐥"
 nouvo_chenn = " ".join([mo[0].upper()+ mo[1:] for mo in chenn.split])
-print(nouvo_chenn)
+print("mete tout premye lèt chak mo en majiskil:",nouvo_chenn)
 
 chenn = "Maman a remis tous ses devoirs"
 mo_komanse = "".join([mo[0] for mo in chenn.split()])
-print(mo_komanse)
+print("rekipere premye lèt chak mo epi afiche yo",mo_komanse)
 
 chenn = "Haiti est un pays montagneux"
 nouvo_chenn = chenn.replace("a","@")
@@ -48,4 +48,31 @@ chenn_san_espas =chenn.replace("","")
 kantite_karaktè = str(len(chenn))
 nouvo_chenn = chenn_san_espas + kantite_karaktè
 print(nouvo_chenn)
+
+print("partie 2 : Master List Union & Intersection & lis comprehension")
+
+n = 40
+lis_divizib = [i for i in range(n+1) if i % 2]
+print("kreye yon lis eleman ki divizib pa 2,nan entèval[0-N]:",lis_divizib)
+
+liste_entier = [1, 2, 3,4 ,5]
+liste_chaine =[str(x) for x in liste_entier]
+print("konvèti lis antye an yon lis chenn:",liste_chaine)
+
+
+liste_chaine_miniscule =["janvier","fevrier","mars","avril","mai"]
+liste_chaine_majuscule = [mo.upper() for mo in liste_chaine_miniscule]
+print("konvèti lis chenn majiskil en miniskil:",liste_chaine_majuscule)
+
+liste =[10,20,30,40,50,60,70,80,90]
+nouvo_liste = [elem for i,elem in enumerate(lis) if i % 3 == 0]
+print("kreye yon lis ki divizib pa 3 yo selman:",nouvo_liste)
+
+liste_element =[1, 2, 3, 4, 5, 6, 7, 8, 9]
+nouvo_liste =[tuple(liste_element[i:i+3]) for i in range(0,len(liste_element),3)]
+print("liste ki gn chak 3 eleman yo nan yon TIPL:",nouvo_liste)
+
+
+
+
 
